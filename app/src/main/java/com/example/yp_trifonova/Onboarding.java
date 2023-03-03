3db7a0793f13e5a19e630e81197f54b0f2b584b4
Настя Ким <nastytrifono03@gmail.com>
2023-03-03 15:57:15 +0300

    Sign in

--- app/src/main/java/com/example/yp_trifonova/Onboarding.java
+++ app/src/main/java/com/example/yp_trifonova/Onboarding.java
@@ -2,7 +2,9 @@ package com.example.yp_trifonova;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Onboarding extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class Onboarding extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_onboarding);
+    }
+    public void Entrance(View v) {
+        Intent intent = new Intent( this, Login.class);
+        startActivity(intent);
     }
 }
